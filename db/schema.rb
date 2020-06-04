52baa3ab0a7fcf0d8e465b9aafe0922b942bbdbd
EmirVatric <vatricemir@gmail.com>
2020-06-04 17:01:57 +0200

    integer limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_144210) do
+ActiveRecord::Schema.define(version: 2020_06_04_150002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_144210) do
   end
 
   create_table "queries", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.string "act_identifier"
     t.string "query"
     t.boolean "found"
