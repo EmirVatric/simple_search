716fd7bf687213f24348daf9a43363e52260ca09
EmirVatric <vatricemir@gmail.com>
2020-06-04 16:56:47 +0200

    background remove

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,7 +12,7 @@ class SearchController < ApplicationController
   private
 
   def search_service
-    @search_service ||= SearchService.new(permitted_params, "#{request.remote_ip}".gsub(/[^0-9A-Za-z]/, '').to_i)
+    @search_service ||= SearchService.new(permitted_params, "#{request.remote_ip}"[0, 5].gsub(/[^0-9A-Za-z]/, '').to_i)
   end
 
   def permitted_params
